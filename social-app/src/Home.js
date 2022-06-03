a599b6ccfbc3c1f8d9439efd5521c8b107cdac05
Balch95 <lukasz.wozniak95@hotmail.com>
2022-06-03 12:30:12 +0200

    1

--- social-app/src/Home.js
+++ social-app/src/Home.js
@@ -1,17 +1,63 @@
 import * as React from "react";
+import axios from 'axios';
 import { Routes, Route, Link } from "react-router-dom";
+import {useState, useEffect} from "react";
 
-function Home() {
+import PostList from "./PostList";
+
+
+const Home = (props)=>{
+
+   const [postList, setPost] = useState([])
+
+    let postData ={
+      username: "Jan",
+      password: "password"
+    }
+
+    let axiosConfing={
+      headers:{
+        'Content-Type' :'application/json',
+        'Accept' : 'application/json' 
+      }
+    };
+
+    const postDown = () =>{
+      axios.post(
+          'https://akademia108.pl/api/social-app/post/latest',
+          postData,
+          axiosConfing)
+        .then((res)=>{
+            setPost(()=>{
+              let newPostObj = {
+                id: res.data.id,
+                data: res.data.crated_at,
+                content: res.data.content,
+                user: res.data.user,
+              };
+            }); 
+          })
+        .catch((err)=>{
+            console.log('Axios error', err);
+          })
+      };
+   
+
+    useEffect(()=>{
+
+      let timer = setInterval(()=>{postDown()}, 5000);
+
+      return(()=>{
+          clearInterval(timer);
+      });
+      });
+
+    
+
     return (
-      <>
-        <main>
-          <h2>Welcome to the homepage!</h2>
-          <p>You can do this, I believe in you.</p>
-        </main>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </>
+        <div className="main-home">
+            <PostList postList={postList}/>
+        </div>
     );
   }
 
---
+++ social-app/src/PostList.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {useState, useEffect} from "react";
+
+function PostList (props){
+
+    let post = props.postList;
+
+    // let liElement = post.map()
+
+
+    console.log(post);
+
+
+
+
+
+    return (
+        <div className="post-list">
+
+        </div>
+    );
+};
+
+export default PostList;
